2170d9f693badc23bd894d8ccc3220e98ea45c1a
Masakazu Kadoshita <mkadoshita@gmail.com>
2020-02-09 23:56:43 +0900

    use lambda exp in ABC154 problem-D

--- ABC15x/ABC154/d.cpp
+++ ABC15x/ABC154/d.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
 using namespace std;
-typedef long long ll;
 #define rep(i, n) for(int i = 0; i < (n); ++i)
-#define all(x) (x).begin(), (x).end()
-#define dbg(x) cerr << #x << ": " << x << endl
+
 int main() {
   int n, k;
   cin >> n >> k;
   vector<int> p(n);
   rep(i, n) cin >> p[i];
-  int m = n - k + 1;
   double ans = 0.0;
-  rep(i, k) ans += ((double)p[i] + 1) / 2;
+  auto E = [&](int x) { return (double)(x + 1) / 2; };
+  rep(i, k) ans += E(p[i]);
   double now = ans;
   for(int i = 0; i + k < n; i++) {
-    now += ((double)p[i + k] + 1) / 2;
-    now -= ((double)p[i] + 1) / 2;
+    now += E(p[i + k]) - E(p[i]);
     ans = max(ans, now);
   }
   printf("%.10f\n", ans);
