89ceee5637d997f48531ad7971683ddcb48d220c
Masakazu Kadoshita <mkadoshita@gmail.com>
2020-02-18 16:11:56 +0900

    ABC155 problem-D cheat

---
+++ ABC15x/ABC155/d.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+#define all(x) (x).begin(), (x).end()
+#define dbg(x) cerr << #x << ": " << x << endl
+
+const ll INF = (ll)(1e18) + 1;
+const int MAXN = 200005;
+vector<ll> a(MAXN);
+bool f(ll x, ll k) {
+  ll sum = 0LL;
+  int n = a.size();
+  rep(i, n) {
+    if(a[i] < 0) {
+      int ng = -1, ok = n;
+      while(abs(ok - ng) > 1) {
+        int mid = (ng + ok) / 2;
+        if(a[mid] * a[i] < x)
+          ok = mid;
+        else
+          ng = mid;
+      }
+      sum += n - ok;
+    } else {
+      int ok = -1, ng = n;
+      while(abs(ok - ng) > 1) {
+        int mid = (ng + ok) / 2;
+        if(a[mid] * a[i] < x)
+          ok = mid;
+        else
+          ng = mid;
+      }
+      sum += ok + 1;
+    }
+    if(a[i] * a[i] < x) sum--;
+  }
+  sum >>= 1;
+  return sum < k;
+}
+
+int main() {
+  int n;
+  ll k;
+  cin >> n >> k;
+  a.resize(n);
+  rep(i, n) cin >> a[i];
+  sort(all(a));
+  ll ok = -INF, ng = INF;
+  while(abs(ok - ng) > 1) {
+    ll mid = (ok + ng) / 2;
+    if(f(mid, k))
+      ok = mid;
+    else
+      ng = mid;
+  }
+  cout << ok << endl;
+  return 0;
+}
