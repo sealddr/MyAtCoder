e69ec1f9f33322a99b6acf4232f13d3a0d00e5cd
Masakazu Kadoshita <mkadoshita@gmail.com>
2020-02-18 14:02:29 +0900

    ABC155 problem-B simplified the conditions.

--- ABC15x/ABC155/b.cpp
+++ ABC15x/ABC155/b.cpp
@@ -1,19 +1,13 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
-typedef long long ll;
 #define rep(i, n) for(int i = 0; i < (n); ++i)
-#define all(x) (x).begin(), (x).end()
-#define dbg(x) cerr << #x << ": " << x << endl
 int main() {
   int n;
   cin >> n;
   rep(i, n) {
     int a;
     cin >> a;
-    if(a % 2 == 1 || a % 6 == 0 || a % 10 == 0)
+    if(a % 2 == 1 || a % 3 == 0 || a % 5 == 0)
       continue;
     else {
       cout << "DENIED" << endl;
