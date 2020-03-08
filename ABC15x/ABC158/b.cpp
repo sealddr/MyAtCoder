6a4b1e004370e376d3245ac987571a26fb003e07
SEAL <mkadoshita@gmail.com>
2020-03-08 09:14:42 +0900

    ABC158 problem-B,C rework

--- ABC15x/ABC158/b.cpp
+++ ABC15x/ABC158/b.cpp
@@ -1,17 +1,14 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 typedef long long ll;
-#define rep(i, n) for(int i = 0; i < (n); ++i)
-#define all(x) (x).begin(), (x).end()
-#define dbg(x) cerr << #x << ": " << x << endl
 int main() {
   ll n, a, b;
   cin >> n >> a >> b;
-  cout << a * (n / (a + b))
-            + min(a, n - (a + b) * (n / (a + b)))
-       << endl;
+  ll k = n / (a + b);
+  ll nb = a * k;
+  ll rest = n - (a + b) * k;
+  nb += min(rest, a);
+  cout << nb << endl;
   return 0;
 }
--- ABC15x/ABC158/c.cpp
+++ ABC15x/ABC158/c.cpp
@@ -1,16 +1,10 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
-typedef long long ll;
-#define rep(i, n) for(int i = 0; i < (n); ++i)
-#define all(x) (x).begin(), (x).end()
-#define dbg(x) cerr << #x << ": " << x << endl
 int main() {
   int a, b;
   cin >> a >> b;
-  for(int x = 1; x <= 1250; x++) {
+  const int MAX_X = 1250;
+  for(int x = 1; x <= MAX_X; x++) {
     if((8 * x) / 100 == a && (10 * x) / 100 == b) {
       cout << x << endl;
       return 0;
