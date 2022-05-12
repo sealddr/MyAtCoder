52f3e6c6afc3f53b6fa5b794a75aced8e95c0daf
masakazu.kadoshita <mkadoshita@gmail.com>
2022-05-12 22:43:14 +0900

    ABC250 Problem D using eratosthenes lib

--- ABC25x/ABC250/d.cpp
+++ ABC25x/ABC250/d.cpp
@@ -2,32 +2,32 @@
 
 using namespace std;
 typedef long long ll;
-ll MAXQ = 1000000;
-ll arr[1000005];
-ll cnt[1000005];
-void eratosthenes() {
-    for(int i = 0; i < MAXQ; i++) {
-        arr[i] = 1;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define MAXQ 1000000
+
+vector<int> eratosthenes(int n) {
+    vector<int> parr(n + 1);
+    parr[0] = 0;
+    parr[1] = 0;
+    for(int i = 2; i <= n; i++) parr[i] = 1;
+    for(int i = 2; i * i <= n; i++) {
+        if(!parr[i]) continue;
+        for(int j = 0; i * (j + 2) <= n; j++)
+            parr[i * (j + 2)] = 0;
     }
-    for(int i = 2; i < sqrt(MAXQ); i++) {
-        if(arr[i]) {
-            for(int j = 0; i * (j + 2) < MAXQ; j++) {
-                arr[i * (j + 2)] = 0;
-            }
-        }
-    }
-    cnt[2] = arr[2];
-    for(int i = 2; i < MAXQ; i++)
-        cnt[i + 1] += cnt[i] + arr[i + 1];
+    return parr;
 }
 
 int main() {
+    vector<int> parr = eratosthenes(MAXQ);
+    vector<ll> cnt(MAXQ + 1);
+    for(int i = 1; i <= MAXQ; i++)
+        cnt[i] = cnt[i - 1] + parr[i];
     ll n;
     cin >> n;
     ll ans = 0;
-    eratosthenes();
-    for(ll q = 2; q < MAXQ && 1 < n / (q * q * q); q++) {
-        if(!arr[q]) continue;
+    for(ll q = 2; q * q * q <= n; q++) {
+        if(!parr[q]) continue;
         ll p = n / (q * q * q);
         if(p >= q) p = q - 1;
         ans += cnt[p];
